4e730974cb3d2566317281dc79e06e3ae04fba02
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-19 19:14:52 +0530

    express
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-import { express } from 'express';
+import express from 'Express';
 
 const router = express.Router();
 
@@ -8,12 +8,21 @@ const users = [
 		last_name: 'Doe',
 		age: 25,
 	},
+	{
+		first_name: 'Jane',
+		last_name: 'Doe',
+		age: 21,
+	},
 ];
 
 // all routes here are starting with /users
 router.get('/', (req, res) => {
 	console.log(users);
 	res.send('Hello');
+});
+
+router.post('/', (req, res) => {
+	console.log('post');
 });
 
 export default router;
