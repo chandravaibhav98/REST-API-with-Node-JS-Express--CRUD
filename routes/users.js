13ad495259456b5e817ddb71f342f10e22b39296
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-19 23:44:51 +0530

    controllers->routes
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- routes/users.js
+++ routes/users.js
@@ -1,55 +1,23 @@
-import express from 'Express';
-import { v4 as uuidv4 } from 'uuid';
-// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+import express from 'express';
+
+import {
+	getUsers,
+	createUser,
+	getUser,
+	deleteUser,
+	updateUser,
+} from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [];
+router.get('/', getUsers);
 
-// GET/users
-router.get('/', (req, res) => {
-	console.log(users);
-	res.send(users);
-});
+router.post('/', createUser);
 
-// POST/users
-router.post('/', (req, res) => {
-	console.log(req.body);
-	const user = req.body;
-	const userWithID = { ...user, id: uuidv4() };
-	users.push(userWithID);
-	res.send(`User with the Name ${user.first_name} added to the database`);
-});
+router.get('/:id', getUser);
 
-// GET/users/:id
-router.get('/:id', (req, res) => {
-	const { id } = req.params;
-	const userFound = users.find((user) => user.id === id);
-	res.send(userFound);
-});
+router.delete('/:id', deleteUser);
 
-// DELETE/users/:id
-router.delete('/:id', (req, res) => {
-	const { id } = req.params;
-	users = users.filter((user) => user.id !== id);
-	res.send(`User with the id ${id} deleted from the database`);
-});
-
-// UPDATE/users/:id
-router.patch('/:id', (req, res) => {
-	const { id } = req.params;
-	const { firstName, lastName, age } = req.body;
-	const user = users.find((user) => user.id === id);
-	if (firstName) {
-		user.firstName = firstName;
-	}
-	if (lastName) {
-		user.lastName = lastName;
-	}
-	if (age) {
-		user.age = age;
-	}
-	res.send(`User with the id ${id} has been updated`);
-});
+router.patch('/:id', updateUser);
 
 export default router;
